3b404f30b1c13e14e26b2155a09ec11b6b0e6de3
Matheus T. dos Santos <matheuswhite@citrinio.com>
2025-09-22 10:52:06 -0300

    refactor: remove outras partes para apenas o binário

--- src/main.rs
+++ src/main.rs
@@ -1,79 +1,44 @@
-use crate::{genetic_algorithm::GeneticAlgorithmBuilder, individual::Individual, report::Report};
-
-mod genetic_algorithm;
-mod individual;
-mod input;
-mod population;
-mod report;
-mod work;
-
-pub fn progress_bar(id: String, current: usize, total: usize) {
-    println!(
-        "[{}] [{}>{}] {}%",
-        id,
-        "=".repeat(current * 20 / total),
-        " ".repeat((total - current) * 20 / total),
-        current * 100 / total
-    );
-}
+use aule::prelude::*;
+use ndarray::Array2;
+use std::f32::consts::PI;
 
 fn main() {
-    // let mut report = Report::new("report.pdf");
-    // let individual = Individual::new(0.03, 2.75, 0.28);
-    // report.set_best_individual(individual);
-    // report.save();
+    let args = std::env::args().collect::<Vec<String>>();
+    let kp = args
+        .get(1)
+        .and_then(|s| s.parse::<f32>().ok())
+        .unwrap_or(1.0);
+    let ki = args
+        .get(2)
+        .and_then(|s| s.parse::<f32>().ok())
+        .unwrap_or(0.0);
+    let kd = args
+        .get(3)
+        .and_then(|s| s.parse::<f32>().ok())
+        .unwrap_or(0.0);
 
-    genetic_algorithm_example();
-}
+    let k = 1.0;
+    let a = 1.0;
+    let time = Time::from((1e-4, 2.0 * PI));
 
-fn genetic_algorithm_example() {
-    println!("Generating initial population...");
+    let mut input = Sinusoid::new(1.0, 1.0 / (2.0 * PI), 0.0);
+    let mut good_hart = GoodHart::new(0.3, 0.3, 0.4);
+    let mut pid = PID::new(kp, ki, kd);
+    let mut plant: SS<RK4> = SS::new(
+        Array2::from_shape_vec((1, 1), vec![-a * k]).unwrap(),
+        vec![k],
+        vec![1.0],
+        0.0,
+    );
 
-    let mut report = Report::new("report.pdf");
-    let mut ga = GeneticAlgorithmBuilder::default()
-        .with_population_size(5_000)
-        .with_parallel_works(4)
-        .build();
+    for dt in time {
+        let signal = dt >> input.as_input();
+        let error = signal - plant.last_output();
+        let control_signal = pid.as_siso() * error;
+        let _output = control_signal * plant.as_siso();
 
-    let mut best_individual = None;
-
-    loop {
-        println!("Evolving generation {}", ga.generation());
-        let Some(best) = ga.eval(0.25, 0.1) else {
-            println!("No best individual found in this generation.");
-            break;
-        };
-
-        best_individual = Some(best.clone());
-        report.set_best_individual(best.clone());
-
-        println!(
-            "Generation {}:\n  Size: {}\n  Best PID = (kp: {:.2}, ki: {:.2}, kd: {:.2}) with fitness {:.5}",
-            ga.generation(),
-            ga.len(),
-            best.kp(),
-            best.ki(),
-            best.kd(),
-            best.fitness()
-        );
-
-        if best.fitness() <= 3.5 {
-            println!("Stopping criteria reached.");
-            break;
-        }
+        let _ = (error, control_signal) >> good_hart.as_error_metric();
     }
 
-    if let Some(best) = best_individual {
-        println!(
-            "Best individual found: PID = (kp: {:.2}, ki: {:.2}, kd: {:.2}) with fitness {:.5}",
-            best.kp(),
-            best.ki(),
-            best.kd(),
-            best.fitness()
-        );
-        report.save();
-        best.show();
-    } else {
-        println!("No best individual found.");
-    }
+    println!("{}", good_hart.value());
 }
